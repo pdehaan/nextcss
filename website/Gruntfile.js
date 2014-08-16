c5193ae92f8771205f632a57afc34523bffc9cdc
myfreeweb <floatboth@me.com>
2014-08-17 01:52:23 +0400

    fix grunt

--- website/Gruntfile.js
+++ website/Gruntfile.js
@@ -1,19 +1,31 @@
+'use strict';
+
 module.exports = function(grunt) {
 	grunt.loadNpmTasks('grunt-broccoli');
+	grunt.loadNpmTasks('grunt-gh-pages');
+	grunt.loadNpmTasks('grunt-contrib-clean');
 
 	grunt.initConfig({
 		// pkg: grunt.file.readJSON('package.json'),
 		broccoli: {
 			dist: {
-				dest: 'build',
+				dest: './build',
 				config: function() {
 					require('sweet.js').loadMacro('sweetbuild');
 					return require('./Brocfile.sjs');
 				}
 			}
+		},
+		clean: ['./tmp', './build'],
+		'gh-pages': {
+			options: {
+				base: './build'
+			},
+			src: ['**']
 		}
 	});
 
 	grunt.registerTask('serve', ['broccoli:dist:serve']);
-	grunt.registerTask('default', ['broccoli:dist:build']);
+	grunt.registerTask('default', ['clean', 'broccoli:dist:build']);
+	grunt.registerTask('publish', ['default', 'gh-pages']);
 }
